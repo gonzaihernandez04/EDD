028756d690c1e8eebfbdbc563b8aa96c1b1adf1f
gonzalo <hernandezgonzalo584@gmail.com>
2025-10-20 13:05:11 -0300

    eval api

--- 22.Redes sociales/facebook.py
+++ 22.Redes sociales/facebook.py
@@ -3,13 +3,22 @@ import os
 import dotenv
 import facebook
 
+
+
+def validarToken(token):
+    try:
+            response = requests.get(f"https://graph.facebook.com/me?access_token={token}")
+            print("Token valido, OK",response.status_code)
+    
+    except requests.exceptions.HTTPError:
+            print("Error al obtener API facebook. Token invalido")
+    except requests.exceptions.ConnectionError as errc:
+        print(f"Error Connecting: {errc}")
+    except requests.exceptions.Timeout as errt:
+        print(f"Timeout Error: {errt}")
+
 dotenv.load_dotenv()
 
 token = os.getenv("TOKEN_FACEBOOK")
-response = requests.get(f"https://graph.facebook.com/me?access_token={token}")
-
-if response.status_code == 200:
-    print("✓ Token válido:", response.json())
-else:
-    print("✗ Error:", response.text)
+validarToken(token)
 
